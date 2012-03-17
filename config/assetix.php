ec21c8a31267a2893ef3e8102e41c3897fd8ff08
Keith Loy <keith.loy@cwlake.com>
2012-03-17 00:22:05 -0500

    added comments to config file

--- config/assetix.php
+++ config/assetix.php
@@ -2,19 +2,25 @@
 
 return array(
 	// Debug mode turns off some filters such as yuicompressor.
-	// Set to true for production.
+	// Set to false for production.
 	'debug' => true,
 	// Path to yuicompressor
 	'yuicompressor_path' => ASSETIX_PATH.'/bin/yuicompressor-2.4.7.jar',
 	// Path to nodejs executable
 	'node_path' => '/usr/bin/node',
-	// Path to use for cache
+	// Path to use for asset cache
 	'cache_path' => ASSETIX_PATH.'/cache',
+	// Path to cssembed jar
 	'cssembed_path' => ASSETIX_PATH.'/bin/cssembed-0.4.5.jar',
 	'cssembed_root' => ASSETIX_PATH.'/assets/images',
+	// Javascript namespace to compile templates under
 	'underscore_namespace' => 'JST',
+	// Extension for underscore files
 	'underscore_ext' => '.jst',
+	// Absolute path to output assets to
 	'output_absolute_path' => ASSETIX_ASSET_PATH.'/production',
+	// Web path used for serving links
 	'output_path' => '/assets/production',
+	// Current assets version. Update to break production cache.
 	'assets_version' => '0.0.1',
 );
