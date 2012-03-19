13f06eedcf879da9ac7ca3b795870fd79b0efd3c
Keith Loy <keith.loy@cwlake.com>
2012-03-19 08:51:36 -0500

    replaced shell_exec with unlink in examples.

--- examples/links.php
+++ examples/links.php
@@ -6,9 +6,12 @@ require_once("$base_dir/classes/assetix.php");
 
 use Assetix\Assetix;
 
-// Clear cache for testing purposes
-shell_exec("rm $base_dir/cache/*");
-shell_exec("rm $base_dir/assets/production/*");
+// Clear cache and production assets for testing purposes. Do not do this unless you wish
+// to force recompilation even when an asset has not been changed.
+$cache_path = "$base_dir/cache/*";
+array_map("unlink", glob($cache_path));
+$prod_path = "$base_dir/assets/production/*";
+array_map("unlink", glob($prod_path));
 
 // Instantiate Assetix
 $assetix = new Assetix();
